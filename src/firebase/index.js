5d3df59e45791d143d07ad404976c1a295414801
Spikesito <emileseguret@icloud.com>
2023-12-04 16:21:39 +0100

    Init firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,8 +1,26 @@
-import { initializeApp } from 'firebase/app';
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import { getDatabase } from 'firebase/database'
+import 'firebase/compat/auth' // TODO: Add SDKs for Firebase products that you want to use
+import Credentials from '../../credentials'
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Replace the following with your app's Firebase project configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  //...
-};
+  apiKey: Credentials.apiKey,
+  authDomain: Credentials.authDomain,
+  projectId: Credentials.projectId,
+  storageBucket: Credentials.storageBucket,
+  messagingSenderId: Credentials.messagingSenderId,
+  appId: Credentials.appId,
+}
 
-export const app = initializeApp(firebaseConfig);
+//init firebase app
+let app
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig)
+}
+
+const auth = firebase.auth()
+const db = getDatabase(app)
+export { auth, db }
