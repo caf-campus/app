de82e9213492f5d8a6daf861e1f416c18946614d
sohkey <sohkey.fetch@gmail.com>
2023-12-05 09:41:19 +0100

    update: file refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './components/Login.jsx'
-import Home from './components/Home.jsx'
-import Notfound from './components/404.jsx'
+import Login from './views/Login.jsx'
+import Home from './views/Home.jsx'
+import Notfound from './views/404.jsx'
 import Layout from './components/Layout.jsx'
-import Register from './components/Register.jsx'
+import Register from './views/Register.jsx'
 
 function App() {
   return (
