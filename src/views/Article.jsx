7f705ecf9e0b42e433eaa95366bb108d057d2d5b
LucasHospice <hospice.lucas38@gmail.com>
2023-12-05 15:10:27 +0100

    Nouvelle branche

---
+++ src/views/Article.jsx
@@ -0,0 +1,13 @@
+const Article = () => {
+    return (
+      <>
+        <div className="h-screen w-full flex justify-center items-center flex-col">
+          <h1 className="text-4xl">Article</h1>
+          <p>Ceci est mon article</p>
+        </div>
+      </>
+    )
+  }
+  
+  export default Article
+  
