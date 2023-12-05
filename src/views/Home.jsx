98cf6e06409b4d9390c764ff4de92c1884809150
sohkey <sohkey.fetch@gmail.com>
2023-12-05 12:32:23 +0100

    fix: conflicts

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,12 @@
+import Comment from '../components/Comment'
+
 const Home = () => {
   return (
     <>
       <div className="h-screen w-full flex justify-center items-center flex-col">
         <h1 className="text-4xl">Café Campus</h1>
         <p>ici ça tchatche</p>
+        <Comment></Comment>
       </div>
     </>
   )
