91d2fc4ef3d82192d23f09f8513cf0b1863c9f6c
LoanF <loanfrancois1@gmail.com>
2023-12-07 14:56:46 +0100

    feat: article page backend + edit login color

--- src/views/Article.jsx
+++ src/views/Article.jsx
@@ -1,19 +1,35 @@
+import { useEffect, useState } from 'react'
+import { getArticleByID, getUserByID } from '../core'
+
 const Article = () => {
+  const [article, setArticle] = useState([])
+  const [author, setAuthor] = useState({})
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    const id = urlParams.get('id') // Remplacez 'id' par le nom de votre paramètre
+
+    // Utilisez l'ID pour obtenir les données de l'article
+    getArticleByID(id).then(article => {
+      setArticle(article)
+      // Récupérer l'auteur de l'article
+      getUserByID(article.auteur).then(user => {
+        setAuthor(user)
+      })
+    })
+  }, [])
+
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-md max-w-2xl w-full mt-28">
-        <h1 className="text-4xl font-bold mb-4 text-indigo-700">
-          Lorem ipsum dolor sit amet
+    <div className="flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md min-w-[70%] max-w-[70%] m-10">
+        <h1 className="text-4xl font-bold mb-4 text-grey-700">
+          {article.titre}
         </h1>
-        <p className="text-gray-600 mb-6">
-          Consectetur adipiscing elit. Praesent suscipit arcu velit.
-        </p>
+        <p className="text-gray-600 mb-6">{article.description}</p>
         <div className="prose max-w-none text-gray-800">
           <p>
-            Contenu de larticle. Lorem ipsum dolor sit amet, consectetur
-            adipiscing elit. Praesent suscipit arcu velit. Blablabla...
+            <b>Par {author.pseudo}</b> - <i>{article.date}</i>
           </p>
-          <p>Ajouter plus de contenu ici.</p>
         </div>
       </div>
     </div>
