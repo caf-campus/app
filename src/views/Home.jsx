4a4bea9f087f8beb0860ceb2d725bfb994693a66
LoanF <loanfrancois1@gmail.com>
2023-12-07 10:09:48 +0100

    feat: add articles to homepage

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,44 +1,58 @@
+import { useEffect, useState } from 'react'
+import { ReadData, getUserByID } from '../core'
+
 const Home = () => {
-  const articles = [
-    {
-      id: 1,
-      title: 'Découverte des cafés du monde',
-      author: 'Café Campus',
-      content:
-        "Aujourd'hui, nous partons à la découverte des cafés du monde entier. Quels sont vos cafés préférés ?",
-    },
-    {
-      id: 2,
-      title: 'Les bienfaits du café sur la productivité',
-      author: 'Café Campus',
-      content:
-        'Le café est-il votre compagnon de travail ? Découvrez comment il peut améliorer votre productivité au quotidien.',
-    },
-    {
-      id: 3,
-      title: 'Recettes de café originales à essayer chez vous',
-      author: 'Café Campus',
-      content:
-        'Envie de changer votre routine caféinée ? Essayez ces recettes de café créatives et savoureuses à la maison.',
-    },
-  ]
+  const [articles, setArticles] = useState([])
+  const [authors, setAuthors] = useState({})
+
+  useEffect(() => {
+    ReadData('articles')
+      .then(data => {
+        const articlesArray = Object.keys(data).map(key => ({
+          id: key,
+          ...data[key],
+        }))
+
+        // Récupérer tous les auteurs
+        return Promise.all(
+          articlesArray.map(article =>
+            getUserByID(article.auteur).then(author => ({
+              [article.auteur]: author,
+            })),
+          ),
+        ).then(authorsArray => {
+          const authorsObject = Object.assign({}, ...authorsArray)
+          setArticles(articlesArray)
+          setAuthors(authorsObject)
+        })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
 
   return (
-    <div className="h-screen w-full flex justify-center items-center flex-col border-2 border-gray-300 p-8">
+    <div className="h-screen w-full flex justify-center items-center flex-col p-8">
       <h1 className="text-4xl">Café Campus</h1>
       <p>ici ça tchatche</p>
 
       <div className="mt-8 w-[60%]">
-        {/* Affiche la liste des articles */}
         {articles.map(article => (
-          <div
-            key={article.id}
-            className="bg-white p-6 mb-4 rounded-md shadow-md border border-gray-300"
-          >
-            <h2 className="text-2xl font-semibold mb-2">{article.title}</h2>
-            <p className="text-gray-700">{article.content}</p>
-            <p className="text-gray-500 text-sm mt-2">Par {article.author}</p>
-          </div>
+          <a href={`article?id=${article.id}`} key={article.id}>
+            <div className="bg-white p-6 mb-4 rounded-md shadow-md border border-gray-300">
+              <h2 className="text-2xl font-semibold mb-2">{article.titre}</h2>
+              <p className="text-gray-700">
+                {article.description.length > 100
+                  ? `${article.description.substring(0, 100)}...`
+                  : article.description}
+              </p>
+              {authors[article.auteur] && (
+                <p className="text-gray-500 text-sm mt-2">
+                  Par {authors[article.auteur].pseudo} - <i>{article.date}</i>
+                </p>
+              )}
+            </div>
+          </a>
         ))}
       </div>
     </div>
