df7733083fdceb3e6b38b9d0140345a92a468c0d
LucasHospice <hospice.lucas38@gmail.com>
2023-12-07 10:15:39 +0100

    article page - fix conflicts

--- src/views/Article.jsx
+++ src/views/Article.jsx
@@ -1,13 +1,23 @@
+import React from 'react';
+
 const Article = () => {
-    return (
-      <>
-        <div className="h-screen w-full flex justify-center items-center flex-col">
-          <h1 className="text-4xl">Article</h1>
-          <p>Ceci est mon article</p>
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md max-w-2xl w-full mt-28">
+        <h1 className="text-4xl font-bold mb-4 text-indigo-700">Lorem ipsum dolor sit amet</h1>
+        <p className="text-gray-600 mb-6">Consectetur adipiscing elit. Praesent suscipit arcu velit.</p>
+        <div className="prose max-w-none text-gray-800">
+          <p>
+            Contenu de l'article. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Praesent suscipit arcu velit. Blablabla...
+          </p>
+          <p>
+            Ajouter plus de contenu ici.
+          </p>
         </div>
-      </>
-    )
-  }
-  
-  export default Article
-  
+      </div>
+    </div>
+  );
+};
+
+export default Article;
