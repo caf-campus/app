01917beea4419eda129b375578f68e16a65d9d8d
JeanPhilippeCaetano <caetano.jphilippe@gmail.com>
2023-12-07 12:11:50 +0100

    update: update+isAdmin + rebase develop with all articles

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,16 +2,17 @@
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app'
 import { getDatabase } from 'firebase/database'
+import Credentials from '../../credentials'
 import 'firebase/compat/auth' // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  apiKey: Credentials.apiKey,
+  authDomain: Credentials.authDomain,
+  projectId: Credentials.projectId,
+  storageBucket: Credentials.storageBucket,
+  messagingSenderId: Credentials.messagingSenderId,
+  appId: Credentials.appId,
+  databaseURL: Credentials.databaseURL,
 }
 
 //init firebase app
