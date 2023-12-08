251a5f4963db43de2dd3bbffbf34b29df809ae8d
JeanPhilippeCaetano <caetano.jphilippe@gmail.com>
2023-12-08 13:14:29 +0100

    fix

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,24 +1,26 @@
 // Import the functions you need from the SDKs you need
 import firebase from 'firebase/compat/app'
 import { getDatabase } from 'firebase/database'
-import Credentials from '../../credentials'
 import 'firebase/compat/auth' // TODO: Add SDKs for Firebase products that you want to use
 
-const firebaseConfig = {
-  apiKey: Credentials.apiKey,
-  authDomain: Credentials.authDomain,
-  projectId: Credentials.projectId,
-  storageBucket: Credentials.storageBucket,
-  messagingSenderId: Credentials.messagingSenderId,
-  appId: Credentials.appId,
-  databaseURL: Credentials.databaseURL,
+const useCreds = () => {
+  const config = import.meta.env
+  const creds = {
+    apiKey: config.VITE_apiKey,
+    appId: config.VITE_appId,
+    authDomain: config.VITE_authDomain,
+    databaseURL: config.VITE_databaseURL,
+    messagingSenderId: config.VITE_messagingSenderId,
+    projectId: config.VITE_projectId,
+    storageBucket: config.VITE_storageBucket,
+  }
+  return firebase.initializeApp(creds)
 }
 
-//init firebase app
-const app = firebase.initializeApp(firebaseConfig, {
-  persistence: firebase.browserLocalPersistence,
-})
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const app = useCreds()
 
 const auth = firebase.auth()
 const db = getDatabase(app)
+
 export { auth, db }
